1af284285b15369e2be9602cb61702a55de0899a
Tony Espy <espy@canonical.com>
2020-01-16 11:12:09 -0500

    Add copyright notices to source files
    Signed-off-by: Tony Espy <espy@canonical.com>

--- lib.c
+++ lib.c
@@ -1,3 +1,20 @@
+/* -*- Mode: C; indent-tabs-mode: nil; tab-width: 4 -*-
+ *
+ * Copyright (C) 2019 Canonical, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef _GNU_SOURCE
 #define _GNU_SOURCE
 #endif
@@ -7,6 +24,19 @@
  * $ LD_PRELOAD=./mylib.so ...
  */
 
+/* This code is based on example example code published om launchpad:
+ *
+ * - https://git.launchpad.net/~jdstrand/+git/test-setgroups
+ *
+ * It's meant as a temporary solution for the Fuji version of the edgexfoundry
+ * snap, until the following PR (based on this code) lands in snap-preload:
+ *
+ * https://github.com/sergiusens/snapcraft-preload/pull/39
+ *
+ * The Fuji snap originally used gosu command to run postgres commands as the
+ * 'snap_daemon' user, but as goso doesn't support the extrausers passwd db
+ * extension used on Ubuntu Core, the snap couldn't be installed on a Core system.
+*/
 #include <dlfcn.h>
 #include <stdio.h>
 #include <grp.h>
